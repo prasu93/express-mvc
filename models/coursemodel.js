0de4cd21758b914763f51cecb407572efd15d779
prasu93 <prasannamahesh93@gmail.com>
2019-11-06 15:16:53 -0600

    Added required files for course

---
+++ models/coursemodel.js
@@ -0,0 +1,49 @@
+/**
+*  Developer model
+*  Describes the characteristics of each attribute in a developer resource.
+*
+* @author Denise Case <denisecase@gmail.com>
+* @requires mongoose
+*
+*/
+const mongoose = require('mongoose')
+
+const DeveloperSchema = new mongoose.Schema({
+
+  _id: {
+    type: Number,
+    required: true
+  },
+  SchoolNumber: {
+    type: String,
+    minlength: 2,
+    maxlength: 2,
+    required: true,
+    default: "44"
+  },
+  CourseNumber: {
+    type: String,
+    minlength: 3,
+    maxlength: 3,
+    required: true
+  },
+  Name: {
+    type: String,
+    minlength: 3,
+    maxlength: 100,
+    required: true
+  },
+  inSpring: {
+    type: Boolean,
+    required: true
+  },
+  inSummer: {
+    type: Boolean,
+    required: true
+  },
+  inFall: {
+    type: Boolean,
+    required: true
+  }
+})
+module.exports = mongoose.model('Developer', DeveloperSchema)
