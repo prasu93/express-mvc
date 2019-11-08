afbc95c58762f785ae0ab0fe0d6118111d2291d9
Deepakmalempati <deepakmalempati@gmail.com>
2019-11-08 11:36:32 -0600

    added students view folder

---
+++ routes/studentrouter.js
@@ -0,0 +1,30 @@
+/**
+ * @router.js - manages all routing
+ *
+ * router.get when assigning to a single request
+ * router.use when deferring to a controller
+ *
+ * @requires express
+ */
+
+const express = require('express')
+
+console.log('START routing')
+const router = express.Router()
+
+// Manage top-level request first
+router.get('/', (req, res, next) => {
+  console.log('Request to /')
+  res.sendFile('index.html')
+})
+
+router.get('/index', (req, res, next) => {
+  console.log('Request to /index')
+  res.sendFile('index.html')
+})
+
+// Defer path requests to a particular controller
+router.use('/dev', require('../controllers/studentcontroller.js'))
+
+console.log('END routing')
+module.exports = router
